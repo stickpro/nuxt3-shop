7612260988e58044acd06888782570abfe26ad5e
Vladislav Bulgakov <stick.qwe@gmail.com>
2022-02-23 02:12:53 +0300

    add vsticky component

---
+++ components/controls/VSticky/VSticky.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="v-sticky">
+        <div class="v-sticky__container">
+            <slot name="sticky" />
+        </div>
+        <slot />
+    </div>
+</template>
+<style lang="postcss">
+.v-sticky {
+    position: relative;
+
+    &__container {
+        top: 0;
+        position: sticky;
+        z-index: 50;
+    }
+}
+</style>
--- plugins/desing-components.ts
+++ plugins/desing-components.ts
@@ -2,6 +2,7 @@ import { defineNuxtPlugin } from "#app";
 import VSvg from "../components/controls/VSvg/VSvg.vue";
 import VButton from "../components/controls/VButton/VButton.vue";
 import VLink from "../components/controls/VLink/VLink.vue";
+import VSticky from "~~/components/controls/VSticky/VSticky.vue";
 import "virtual:svg-icons-register";
 
 // Global register component
@@ -9,4 +10,5 @@ export default defineNuxtPlugin((nuxtApp) => {
     nuxtApp.vueApp.component("v-svg", VSvg);
     nuxtApp.vueApp.component("v-button", VButton);
     nuxtApp.vueApp.component("v-link", VLink);
+    nuxtApp.vueApp.component("v-sticky", VSticky);
 });
